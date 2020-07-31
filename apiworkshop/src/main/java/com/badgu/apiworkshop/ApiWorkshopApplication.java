4e790a8c220cb1c8cb42ca4d3f331a0bef652099
Tushar Badgu <tushar.badgu@gmail.com>
2020-07-31 15:58:58 +0530

    Lab 3: Open API Specifications
    Signed-off-by: Tushar Badgu <tushar.badgu@gmail.com>

--- apiworkshop/src/main/java/com/badgu/apiworkshop/ApiWorkshopApplication.java
+++ apiworkshop/src/main/java/com/badgu/apiworkshop/ApiWorkshopApplication.java
@@ -2,6 +2,9 @@ package com.badgu.apiworkshop;
 
 import com.badgu.apiworkshop.model.Todo;
 import com.badgu.apiworkshop.repository.TodoRepository;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +20,20 @@ public class ApiWorkshopApplication {
     @Bean
     public CommandLineRunner setup(TodoRepository todoRepository) {
         return args -> todoRepository.save(new Todo("Tushar Badgu", "Sample Todo"));
+    }
+
+    @Bean
+    public OpenAPI customizeOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Todo Appication")
+                        .version("0.0.1-SNAPSHOT")
+                        .description("Sample Application to demonstrate Spring Boot + Open API fetures")
+                        .contact(new Contact()
+                                .name("Tushar Badgu")
+                                .email("demo@email.com")
+                                .url("https://github.com/tbadgu"))
+                );
     }
 
 }
